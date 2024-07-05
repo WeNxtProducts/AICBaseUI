5183a8f5f11fbb555725fd0aa6abc15cf342adfc
WenxtTech <timothygodwin@wenxttech.com>
2024-07-05 18:17:14 +0530

    quote started

--- src/pages/quote/Quote.jsx
+++ src/pages/quote/Quote.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import QuoteHeader from './quoteHeader/QuoteHeader';
 import './Quote.scss';
 
 const Quote = () => {
  return (
   <div className='Quote'>
-   <p>Quote.jsx</p>
+   <QuoteHeader />
   </div>
  );
 };
---
+++ src/pages/quote/quoteHeader/QuoteHeader.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import weNxtLogo from '../../../assets/WeNxt_Logo-removebg.png';
+
+const QuoteHeader = () => {
+ return (
+  <div className='quote_header'>
+   <div className='img_head'>
+    <img className='logo_img p-1' alt='logo' src={weNxtLogo} />
+   </div>
+   <div className='sub_header' />
+  </div>
+ );
+};
+
+export default QuoteHeader;
