d2cf91fdffdd5d88623d105b1c8ed3ebff184b3b
WenxtTech <timothygodwin@wenxttech.com>
2024-07-16 18:14:29 +0530

    quote

--- src/pages/quote/stepper/Stepper.jsx
+++ src/pages/quote/stepper/Stepper.jsx
@@ -20,33 +20,22 @@ const QuoteStepper = () => {
      completedColor: '#0382C8',
      activeColor: '#ffbd13',
      disabledColor: '#eee',
+     stepSize: '2.1em',
+     size: 5,
     }}
-    // styleConfig={{
-    //  labelFontSize: '.85rem',
-    //  activeBgColor: '#ffbd13',
-    //  completedBgColor: '#0382C8',
-    //  inactiveBgColor: '#eee',
-    //  activeTextColor: '#ffffff',
-    //  completedTextColor: '#ffffff',
-    //  inactiveTextColor: '#444',
-    // }}
+    styleConfig={{
+     labelFontSize: '.8rem',
+     borderRadius: '50%',
+     fontWeight: 600,
+    }}
     className='custom_stepper'
     activeStep={activeStep}>
     {quoteStep?.map((item, index) => (
-     <Step
-      className='custom_steps'
-      key={item?.label}
-      // label={
-      //  <p className={`${activeStep > index ? 'completed' : 'not_completed'}`}>
-      //   {item?.label}
-      //  </p>
-      // }
-      label={item?.label}
-     />
+     <Step className='custom_steps' key={item?.label} label={item?.label} />
     ))}
    </Stepper>
-   <button onClick={() => setActiveStep(activeStep + 1)}>+</button>
    <button onClick={() => setActiveStep(activeStep - 1)}>-</button>
+   <button onClick={() => setActiveStep(activeStep + 1)}>+</button>
   </div>
  );
 };
