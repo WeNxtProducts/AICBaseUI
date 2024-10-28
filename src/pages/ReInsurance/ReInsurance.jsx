e4d71a1c08a50e4e4cd82f38a845d67468977c10
WenxtTech <timothygodwin@wenxttech.com>
2024-10-28 16:49:13 +0530

    re-ins redux clear

--- src/pages/ReInsurance/ReInsurance.jsx
+++ src/pages/ReInsurance/ReInsurance.jsx
@@ -1,13 +1,16 @@
-import React, { createContext, useState } from 'react'
+import React, { createContext, useEffect, useState } from 'react'
 import InsuranceHeader from './insuranceHeader/InsuranceHeader';
 import CoverAndInsuranceDetails from './coverAndInsuranceDetails/CoverAndInsuranceDetails';
 import { useSelector } from 'react-redux';
 import EmptyMessage from '../../components/emptyMessage/EmptyMessage';
 import './ReInsurance.scss';
+import { useDispatch } from 'react-redux';
+import { setReInsuranceId, setReInsurancePolNo } from '../../globalStore/slices/ReInsuranceSlices';
 
 export const ReInsuranceContext = createContext();
 
 const ReInsurance = () => {
+    const dispatch = useDispatch();
     const insuranceIDs = useSelector(state => state?.reInsurance);
     const { tranId, rePol } = insuranceIDs
     const [seldAssrCode, setSeldAssrCode] = useState('')
@@ -17,6 +20,13 @@ const ReInsurance = () => {
         tranId, rePol, seldAssrCode, setSeldAssrCode,
         selectedCover, setSelectedCover
     }
+
+    useEffect(() => {
+        return () => {
+            dispatch(setReInsuranceId(''));
+            dispatch(setReInsurancePolNo(''));
+        }
+    }, [])
 
     return (
         <ReInsuranceContext.Provider value={data}>
