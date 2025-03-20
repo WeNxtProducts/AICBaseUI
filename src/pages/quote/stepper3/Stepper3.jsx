10bf40f218a68c4603fb37417ca9e8f9d9660112
WenxtTech <timothygodwin@wenxttech.com>
2025-03-20 14:19:20 +0530

    stepper 3

--- src/pages/quote/stepper3/Stepper3.jsx
+++ src/pages/quote/stepper3/Stepper3.jsx
@@ -1,19 +1,17 @@
-import React, { useState } from 'react'
-import CustomAccordion from '../../../components/customAccordion/CustomAccordion'
+import React, { useState } from 'react';
+import CustomAccordion from '../../../components/customAccordion/CustomAccordion';
 import { Checkbox } from 'antd';
+import CustomerDetails from './customerDetails/CustomerDetails';
+import CustomerAddress from './customerAddress/CustomerAddress';
+import NomineeDetails from './nomineeDetails/NomineeDetails';
 
 const Stepper3 = () => {
-    const [accordionState, setAccordionState] = useState({
-        customerDetails: true,
-        customerAddress: false,
-        nomineeDetails: false,
-    });
+    const [activeSection, setActiveSection] = useState('customerAddress');
 
-    const toggleAccordion = section => {
-        setAccordionState(prevState => ({
-            ...prevState,
-            [section]: !prevState[section],
-        }));
+    const toggleAccordion = (section) => {
+        setActiveSection((prevSection) =>
+            prevSection === section ? null : section
+        );
     };
 
     return (
@@ -21,29 +19,34 @@ const Stepper3 = () => {
             <p className='head_assured_cust'>Assured/Customer Details</p>
             <div>
                 <div className='life_assured_check'>
-                    <Checkbox className='life_check'>Life Assured is premium payor</Checkbox>
+                    <Checkbox className='life_check'>
+                        Life Assured is premium payor
+                    </Checkbox>
                 </div>
                 <CustomAccordion
                     title='Customer Details'
-                    isOpen={accordionState.customerDetails}
+                    isOpen={activeSection === 'customerDetails'}
                     toggleAccordion={() => toggleAccordion('customerDetails')}
-                    content={'Customer Details'}
-                />
+                >
+                    <CustomerDetails />
+                </CustomAccordion>
                 <CustomAccordion
                     title='Customer Address'
-                    isOpen={accordionState.customerAddress}
+                    isOpen={activeSection === 'customerAddress'}
                     toggleAccordion={() => toggleAccordion('customerAddress')}
-                    content={'Customer Address'}
-                />
+                >
+                    <CustomerAddress />
+                </CustomAccordion>
                 <CustomAccordion
                     title='Nominee Details'
-                    isOpen={accordionState.nomineeDetails}
+                    isOpen={activeSection === 'nomineeDetails'}
                     toggleAccordion={() => toggleAccordion('nomineeDetails')}
-                    content={'Nominee Details'}
-                />
+                >
+                    <NomineeDetails />
+                </CustomAccordion>
             </div>
         </div>
-    )
-}
+    );
+};
 
-export default Stepper3
+export default Stepper3;
